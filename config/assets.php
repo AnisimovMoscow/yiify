0f3db8af1e4f6dc4bca5aa560c097eb42f46a364
Anton Anisimov <avanisimov@rttv.ru>
2014-10-27 13:43:25 +0300

    CSS и JS перенесены в папку web, добавлен normalize.css

--- config/assets.php
+++ config/assets.php
@@ -2,22 +2,19 @@
 
 return [
     'bundles' => [
-        'main' => [
-            'class' => 'yii\web\AssetBundle',
-            'sourcePath' => '@app/static',
-            'css' => ['css/main.css'],
-            'js' => ['js/main.js']
-        ],
         'admin' => [
             'class' => 'yii\web\AssetBundle',
-            'sourcePath' => '@app/static',
             'css' => ['css/admin.css'],
             'js' => [],
             'depends' => ['yii\bootstrap\BootstrapAsset']
+        ],        
+        'main' => [
+            'class' => 'yii\web\AssetBundle',
+            'css' => ['css/normalize.css', 'css/main.css'],
+            'js' => ['js/main.js']
         ],
         'index' => [
             'class' => 'yii\web\AssetBundle',
-            'sourcePath' => '@app/static',
             'css' => ['css/index.css'],
             'js' => ['js/index.js'],
             'depends' => ['main']
